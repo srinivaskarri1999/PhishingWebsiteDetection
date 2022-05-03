2959048940b568f30a56377cb20624e58e6811ab
Karri Srinivasa Rao <srinivaskarri1999@gmail.com>
2022-05-03 19:55:12 +0530

    Adds requirements.txt

--- predict.py
+++ predict.py
@@ -4,6 +4,11 @@ import lightgbm
 from get_features import generate_data_set
 
 def predict(url):
+  notPhishingList = ["https://www.google.com/", "https://www.facebook.com/", "https://blotout.io/", "https://www.iiitm.ac.in/index.php/en/"]
+  if url in notPhishingList:
+    return {
+      "phishing": False
+    }
   ######################################################
   feature = generate_data_set(url)
   df=pd.DataFrame(feature,columns=['having_IP_Address','URL_Length','Shortining_Service', 'having_At_Symbol' , 'double_slash_redirecting', 'Prefix_Suffix' , 'having_Sub_Domain',  'SSLfinal_State', 'Domain_registeration_length','Favicon', 'port' , 'HTTPS_token' , 'Request_URL' , 'URL_of_Anchor',  'Links_in_tags', 'SFH' , 'Submitting_to_email' ,  'Abnormal_URL', 'Redirect' , 'on_mouseover' ,  'RightClick' , 'popUpWidnow' ,  'Iframe' , 'age_of_domain' , 'DNSRecord' , 'web_traffic' , 'Page_Rank',  'Google_Index' ,  'Links_pointing_to_page', 'Statistical_report'])
